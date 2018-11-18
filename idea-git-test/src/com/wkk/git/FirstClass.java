d75bc2bb969db239d5a18b402880352eaca433f6
wkk <wkk@163.com>
2018-11-18 17:10:03 +0800

    第二次修改firstclass

--- idea-git-test/src/com/wkk/git/FirstClass.java
+++ idea-git-test/src/com/wkk/git/FirstClass.java
@@ -6,5 +6,7 @@ public class FirstClass {
         System.out.println("22222222222");
         System.out.println("33333333333");
         System.out.println("555555555");
+
+        System.out.println("666666666666");
     }
 }
