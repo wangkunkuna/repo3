7b7f760e6abd72d28fe86bca630a3bf1a39e5564
wkk <wkk@163.com>
2018-11-18 16:48:02 +0800

    添加test工程到git

---
+++ idea-git-test/src/com/wkk/git/FirstClass.java
@@ -0,0 +1,9 @@
+package com.wkk.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+        System.out.println("22222222222");
+        System.out.println("33333333333");
+    }
+}
